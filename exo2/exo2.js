62521bab7125d1e8097d1cec1fdef423319a99cb
Perrinew <106061214+Perrinew@users.noreply.github.com>
2022-07-13 15:06:14 +0200

    1.3

--- exo2/exo2.js
+++ exo2/exo2.js
@@ -1,5 +1,7 @@
-const red = document.querySelector("p")
-red.style.background = "none";
+$(`#paroles .red`).removeClass(`red`);
 
+d3.selectAll(".refrain").classed("italic" , "true");
 
+d3.selectAll("p").style("background-color", "#6495ED");
 
+d3.selectAll("p").style("margin", "0");
